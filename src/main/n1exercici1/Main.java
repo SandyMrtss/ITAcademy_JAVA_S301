2c17778f566448abd6674ca9e03182745b7ff016
SandyMrtss <bebesubi2@hotmail.com>
2023-11-13 09:46:20 +0100

    Initial commit

--- src/main/n1exercici1/Main.java
+++ src/main/n1exercici1/Main.java
@@ -3,8 +3,9 @@ package n1exercici1;
 public class Main {
     public static void main(String[] args) {
         Undo myUndo = Undo.getInstance();
-        for (int i = 1; i < args.length; i++) {
+        for (int i = 0; i < args.length; i++) {
             myUndo.addCommand(args[i]);
         }
+        myUndo.listCommands();
     }
 }
